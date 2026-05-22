866d2077ff56cb0a6293f98e424cb9a454f24dcf
Archu <archu@Archus-MacBook-Air.local>
2026-05-22 15:18:56 -0700

    feat: 🟢 GREEN implementacion de aplicarDescuento regla 2

--- src/producto.js
+++ src/producto.js
@@ -7,5 +7,12 @@ class Producto {
         this.precioBase = precioBase;
         this.descuento = 0;
     }
+
+    aplicarDescuento(porcentaje) {
+        if (porcentaje < 0 || porcentaje > 40) {
+            throw new Error("El descuento debe estar entre 0 y 40%");
+        }
+        this.descuento = porcentaje;
+    }
 }
 module.exports = { Producto };
